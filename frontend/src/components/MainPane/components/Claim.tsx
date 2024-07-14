e7c4d3af59e49a03c5ec95ad469a359a37f865c7
Dmitrii Noskov <dmitrynoskovcz@gmail.com>
2024-07-14 08:06:48 +0200

    add chains

--- frontend/src/components/MainPane/components/Claim.tsx
+++ frontend/src/components/MainPane/components/Claim.tsx
@@ -1,88 +1,56 @@
-"use client";
-import { type FC, type MouseEvent, useEffect, useState } from "react";
-
-import { Button, VStack } from "@chakra-ui/react";
-
-import { useAccount, useContractWrite } from "wagmi";
-
-import { useNotify } from "@/hooks";
+import { FC } from "react";
+import { Button, VStack, useToast } from "@chakra-ui/react";
+import { useWriteContract, useAccount } from "wagmi";
+import { useNotify } from "@/hooks"; 
 
 const contractAddresses = {
-  1: "0xYourMainnetContractAddress",
-  4: "0xYourRinkebyContractAddress",
-  2542: "0x8a67291abf37cf1c168a3786fd63de9e5097dac8"
+    84532: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    421614: "0xf52fd60afac5c14a07c247c045161feabfdc26df",
+    44787: "0x8a67291abf37cf1c168a3786fd63de9e5097dac8",
+    80002: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    2810: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    534351: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    245022926: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    59141: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
+    11155111: "0xf52fD60AfAc5c14A07c247C045161Feabfdc26DF",
 };
 
 const abi = [
-  {
-    "inputs": [],
-    "name": "claim",
-    "outputs": [],
-    "type": "function"
-  }
+    {
+        "inputs": [],
+        "name": "claim",
+        "outputs": [],
+        "type": "function"
+    }
 ];
 
 const Claim: FC = () => {
-  const { connector, isConnected } = useAccount();
-  const { chain } = useAccount();
-  const [isPending, setIsPending] = useState(false);
-  const { notifyError, notifySuccess } = useNotify();
+    const { chain } = useAccount();
+    const { writeContract } = useWriteContract()
 
-  const chainId = chain?.id ?? 1; // Default to Mainnet (1)
-  const contractAddress = contractAddresses[chainId as keyof typeof contractAddresses];
+    const chainId = chain?.id ?? 1; // Default to Mainnet (1)
+    const contractAddress = contractAddresses[chainId as keyof typeof contractAddresses];
 
-  const { data, error, isLoading, isSuccess } = useContractWrite({
-    addressOrName: contractAddress,
-    contractInterface: abi,
-  },
-  'claim'
-  );
-
-  const { hash } = data || {}
-
-  const handleClaim = (e: MouseEvent<HTMLButtonElement>): void => {
-    e.preventDefault();
-    console.log('claim')
-    console.log(data)
-    if (!isConnected) return;
-    console.log('hey')
-  };
-
-  useEffect(() => {
-    if (isLoading) {
-      setIsPending(true);
-    } else {
-      setIsPending(false);
-    }
-
-    if (isSuccess && data) {
-      notifySuccess({
-        title: "Claim successful!",
-        message: `Transaction Hash: ${data.hash}`,
-      });
-    }
-
-    if (error) {
-      notifyError({
-        title: "Claim failed",
-        message: error.message,
-      });
-    }
-  }, [isLoading, isSuccess, data, error, notifyError, notifySuccess]);
-
-  return (
-    <VStack w={"45%"} minWidth={"270px"} gap={2}>
-      <Button
-        variant="ghost"
-        onClick={handleClaim}
-        isLoading={isPending}
-        className="custom-button"
-        disabled={!isConnected}
-      >
-        Claim
-      </Button>
-    </VStack>
-  );
+    return (
+        <VStack w={"45%"} minWidth={"270px"} gap={2}>
+            {/* Optional: Add a chain selector if user can switch chains */}
+            {/* <Select placeholder="Select chain" /> */}
+            <Button
+                variant="ghost"
+                onClick={() => 
+                    writeContract({ 
+                      abi,
+                      address: contractAddress,
+                      functionName: 'claim',
+                   })
+                  }
+                isDisabled={!chain || !contractAddress}
+                className="custom-button"
+            >
+                Claim
+            </Button>
+        </VStack>
+    );
 };
 
 export default Claim;
